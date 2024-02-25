14c1d999bc04fd6b46c6f1de46de2850d9849b65
Devil <69057727+DasBluEyedDevil@users.noreply.github.com>
2024-02-25 17:05:07 -0500

    more column tinkering

--- bbs/commands.py
+++ bbs/commands.py
@@ -57,9 +57,9 @@ class CmdBBS(default_cmds.MuxCommand):
         "List all boards."
         boards = Board.objects.all()
         output = "|b=|n" * 78 + "\n"
-        output += "      |wBoard Name|n".ljust(30)
-        output += "|wLast Post|n".ljust(26)
-        output += "|w# of Messages".ljust(20) + "\n"
+        output += "      |wBoard Name|n".ljust(20)
+        output += "|wLast Post|n".ljust(40)
+        output += "|w# of Messages".rjust(5) + "\n"
         output += "|b=|n" * 78 + "\n"
         for board in boards:
             if board.read_perm == "all" or self.caller.check_permstring(board.read_perm):
