e2f285f3bac5915608807181e286e0ba77a65700
Devil <69057727+DasBluEyedDevil@users.noreply.github.com>
2024-02-25 17:18:08 -0500

    last try

--- bbs/commands.py
+++ bbs/commands.py
@@ -57,8 +57,9 @@ class CmdBBS(default_cmds.MuxCommand):
         "List all boards."
         boards = Board.objects.all()
         output = "|b=|n" * 78 + "\n"
-        output += "      |wBoard Name|n".ljust(34)
-        output += "|wLast Post|n".ljust(20)
+        output += "|wID#|n".ljust(4)
+        output += "|wBoard Name|n".ljust(35)
+        output += "|wLast Post|n".ljust(22)
         output += "|w# of Messages".ljust(13) + "\n"
         output += "|b=|n" * 78 + "\n"
         for board in boards:
