53fa9e0882c6b1a5a413e150b200e4144e101a0a
Devil <69057727+DasBluEyedDevil@users.noreply.github.com>
2024-02-25 17:54:35 -0500

    Update commands.py

--- bbs/commands.py
+++ bbs/commands.py
@@ -364,7 +364,7 @@ def format_board_posts_output(self, posts, board):
     output = "|b=|n" * 78 + "\n"
     output += "|w**** {} ****|n\n".format(board.name).center(78)
     output += "|b=|n" * 78 + "\n"
-    output = "|wID|n".ljust(4)
+    output += "|wID|n".ljust(4)
     output += "  |wTitle|n".ljust(35)
     output += "      |wAuthor|n".ljust(22)
     output += "              |wDate Posted".ljust(13) + "\n"
