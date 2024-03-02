48a7fd3c90d656ecb886143f6d07d7742f5658c5
Devil <69057727+DasBluEyedDevil@users.noreply.github.com>
2024-03-02 12:44:12 -0500

    Update commands.py

--- bbs/commands.py
+++ bbs/commands.py
@@ -57,7 +57,7 @@ class CmdBBS(default_cmds.MuxCommand):
         boards = Board.objects.all()
         output = "|b=|n" * 78 + "\n"
         output += "  |wBoard Name|n".ljust(29)  # Adjusted spacing for new column
-        output += "|wRead Perm|n".ljust(10)  # New column for read permissions
+        output += "|wGroup|n".ljust(10)  # New column for read permissions
         output += "      |wLast Post|n".ljust(22)
         output += "           |w# of Messages".ljust(13) + "\n"
         output += "|b=|n" * 78 + "\n"
