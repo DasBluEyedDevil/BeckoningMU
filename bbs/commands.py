364d000583d53505db264f7d48c48bf877bff3f7
Devil <69057727+DasBluEyedDevil@users.noreply.github.com>
2024-03-02 12:29:19 -0500

    Update commands.py

--- bbs/commands.py
+++ bbs/commands.py
@@ -56,10 +56,10 @@ class CmdBBS(default_cmds.MuxCommand):
         "List all boards."
         boards = Board.objects.all()
         output = "|b=|n" * 78 + "\n"
-        output += "  |wBoard Name|n".ljust(29)  # Adjusted spacing for new column
-        output += "|wGroup|n".ljust(10)  # New column for read permissions
-        output += "      |wLast Post|n".ljust(22)
-        output += "           |w# of Messages".ljust(13) + "\n"
+        output += "  |wBoard Name|n".ljust(29)  # Adjusted width for additional column
+        output += " |wGroup|n".ljust(12)  # Renamed column for read permissions
+        output += "  |wLast Post|n".ljust(17)  # Adjusted width
+        output += " |w# of Messages".ljust(13) + "\n"
         output += "|b=|n" * 78 + "\n"
         for board in boards:
             if board.read_perm == "all" or self.caller.check_permstring(board.read_perm):
@@ -68,13 +68,14 @@ class CmdBBS(default_cmds.MuxCommand):
                 if last_post:
                     formatted_datetime = last_post.created_at.strftime("%Y-%m-%d")
                 num_posts = board.posts.count()
-                read_perm_display = board.read_perm if board.read_perm != "all" else "-"
-                output += "  " + board.name[:24].ljust(29)  # Adjusted spacing for new column
-                output += read_perm_display.ljust(14)  # Display read permission
-                output += formatted_datetime.ljust(22)
+                group_display = board.read_perm if board.read_perm != "all" else " "
+                output += "  " + board.name[:24].ljust(29)  # Adjusted width for additional column
+                output += group_display.ljust(12)  # Display read_perm (now as Group) or space
+                output += formatted_datetime.ljust(17)  # Adjusted width
                 output += str(num_posts).rjust(9) + "\n"
         output += "|b=|n" * 78
         self.caller.msg(output)
+
 
 
     def view_board(self, board):
